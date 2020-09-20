929df1d3071ac38439f8e96a949e8bea3e4d425c
MichailMar <63363137+MichailMar@users.noreply.github.com>
2020-09-20 11:27:35 +0500

    Update core.py

--- vklibary/core.py
+++ vklibary/core.py
@@ -2,7 +2,7 @@ import time
 from python3_anticaptcha import ImageToTextTask
 import vk_api
 from vklibary import database
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def captcha_handler(captcha):
     key = ImageToTextTask.ImageToTextTask(anticaptcha_key="f0d1147d67df6a70b77ccf1e41372dc1", save_format='const') \
@@ -65,6 +65,52 @@ def StaticLoad(api, acid, adids):
         database.InsertStatic(static)
 
 
+def CreateAcc(name, login, password, spent, critik, join):
+    database.Insert("account", "'{0}', '{1}','{2}','{3}','{4}','{5}'".format(name,
+                                                                             login,
+                                                                             password,
+                                                                             spent,
+                                                                             critik,
+                                                                             join))
+
+
+def GetMonitor(start, end):
+    info = {"day": 0, 'info': {}}
+    c = 0
+    for ac in database.GetInfo("account"):
+        c += 1
+        for acv in database.GetInfo("account_monitor", sorting="office_login='{0}'".format(ac['login'])):
+            start_d = datetime.strptime(start, "%Y-%m-%d")
+            end_d = datetime.strptime(end, "%Y-%m-%d")
+            day = start_d - end_d
+            day = -day.days
+            last_day = start_d.today() - timedelta(days=2)
+            last_day = last_day.strftime("%Y-%m-%d")
+            last = {'spent':0, 'join':0}
+            spent = 0
+            join = 0
+            summ = acv["summ"]
+            minimal = ac["minimal_budjet"]
+            pale_join = ac["plane_join"]
+
+            for ad in database.GetInfo("ads", sorting="id_ads_office='{0}'".format(acv["id"])):
+                id = ad["id"]
+                for static_ad in database.GetAds(start, end, "id_ads = '{0}'".format(id)):
+                    spent += static_ad["spent"]
+                    join += static_ad["join"]
+                    if last_day == static_ad["date"]:
+                        last['spent'] += static_ad["spent"]
+                        last['join'] += static_ad["join"]
+
+            max_per = ac["max_waste_per_month"] / 30 * day
+            info['day'] = day
+            info['info'].update({c: {"name":ac["name"],"spent": spent, "join": join, "summ": summ,
+                                  "minimal": minimal, "pale_join": pale_join, "max_per": max_per, "last": last}})
+
+
+    return info
+
+
 def GetStats(start, end):
     static = {}
     for ad in database.GetInfo("ads"):
@@ -95,41 +141,47 @@ def GetStats(start, end):
     return static
 
 
-def CreateAcc(name, login, password, spent, critik, join):
-    database.Insert("account", "'{0}', '{1}','{2}','{3}','{4}','{5}'".format(name,
-                                                                             login,
-                                                                             password,
-                                                                             spent,
-                                                                             critik,
-                                                                             join))
-
-
-def GetMonitor(start, end):
-    info = {"day": 0, 'info': {}}
+def GetCamp(start, end, type):
+    static = {}
     c = 0
-    for ac in database.GetInfo("account"):
-        c += 1
-        for acv in database.GetInfo("account_monitor", sorting="office_login='{0}'".format(ac['login'])):
-            start_d = datetime.strptime(start, "%Y-%m-%d")
-            print(start_d)
-            end_d = datetime.strptime(end, "%Y-%m-%d")
-            day = start_d - end_d
-            day = -day.days
-            spent = 0
+    start_d = datetime.strptime(start, "%Y-%m-%d")
+    end_d = datetime.strptime(end, "%Y-%m-%d")
+    day = start_d - end_d
+    last_day = start_d.today() - timedelta(days=2)
+    last_day = last_day.strftime("%Y-%m-%d")
+    day = -day.days
+    for aci in database.GetInfo("account"):
+        for ac in database.GetInfo("account_monitor", sorting="office_login='{0}'".format(aci['login'])):
             join = 0
-            summ = acv["summ"]
-            minimal = ac["minimal_budjet"]
-            pale_join = ac["plane_join"]
+            clicks = 0
+            spent = 0
+            reach = 0
+            traffic = 0
+            sale = 0
+            join_message = 0
+            name = ""
+            office = ""
+            last = {'spent':0, 'join':0}
+            for ad in database.GetInfo("ads", "id_ads_office='{0}'".format(ac['id'])):
+                if type.lower() in ad["name_camp"].lower():
+                    id = ad["id"]
+                    for static_ad in database.GetAds(start, end, "id_ads = '{0}'".format(id)):
+                        if last_day == static_ad["date"]:
+                            last['spent'] += static_ad["spent"]
+                            last['join'] += static_ad["join"]
+                        clicks += static_ad["clicks"]
+                        spent += static_ad["spent"]
+                        reach += static_ad["reach"]
+                        traffic += static_ad["traffic"]
+                        sale += static_ad["sale"]
+                        join_message += static_ad["join_message"]
+                        join += static_ad["join"]
+                    office = ad['name_office']
+                    name = ad["name_camp"]
 
-            for ad in database.GetInfo("ads", sorting="id_ads_office='{0}'".format(acv["id"])):
-                id = ad["id"]
-                for static_ad in database.GetAds(start, end, "id_ads = '{0}'".format(id)):
-                    spent += static_ad["spent"]
-                    join += static_ad["join"]
 
-            max_per = ac["max_waste_per_month"] / 30 * day
-            info['day'] = day
-            info['info'].update({c: [{"name":ac["name"],"spent": spent, "join": join, "summ": summ,
-                                  "minimal": minimal, "pale_join": pale_join, "max_per": max_per}]})
-
-    return info
+            static.update({c: {'day': day,  'spent': spent,'join': join, 'clicks': clicks, 'traffic': traffic, 'reach': reach,
+                                            'max_per': aci['max_waste_per_month'], 'join_message': join_message, 'name_camp': name,
+                                            'name_office': office, 'sale': sale, 'last': last}})
+            c += 1
+    return static
